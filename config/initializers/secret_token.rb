428713ec33b50a144eac84efe92284b9a0951340
dhoomkarthick <dhoomkarthick@gmail.com>
2014-06-05 12:04:19 +0530

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MultipleImageUploadCarrierwave::Application.config.secret_key_base = '8ce2325c430fecb18a9aa1beb6d355b93adce13a09b46a42ddcd65099b07675325fffc4a19c28263ce83d05da18e932873f375fb597a1ef9e8fd2739098b60ae'
